ea9ac308226f7a35fbd29d0db15a144e7ff4e67b
atamanahmet <ahmtataman@proton.me>
2025-03-20 18:15:35 +0300

    modified:   linkedList.java

--- linkedList.java
+++ linkedList.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,27 +8,16 @@ import org.w3c.dom.Node;
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class ListNode {
-    public int val;
-    public int next;
-
-    public ListNode() {
-    }
-}
 
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        } else if (list2 == null) {
-            return list1;
-        } else if (list1 == null && list2 == null) {
-            return null;
-        }
+        if (list1 == null || list2 == null)
+            return (list1 == null && list2 != null) ? list2 : (list2 == null) ? list1 : null;
+
         ListNode current;
         ListNode otherCurrent;
         ListNode head;
-        boolean check = true;
+
         if (list1.val <= list2.val) {
             current = list1;
             otherCurrent = list2;
@@ -42,17 +29,15 @@ class Solution {
         }
 
         while (current != null) {
+            if (otherCurrent == null) {
+                break;
+            }
             if (current.next == null) {
-                if (otherCurrent == null) {
-                    break;
-                }
                 current.next = otherCurrent;
                 current = otherCurrent;
-                otherCurrent = null;
+                break;
             } else {
-                if (otherCurrent == null) {
-                    break;
-                } else if (current.next.val > otherCurrent.val) {
+                if (current.next.val > otherCurrent.val) {
                     ListNode temp = current.next;
                     current.next = otherCurrent;
                     current = otherCurrent;
