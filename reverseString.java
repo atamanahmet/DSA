31e7cb12816deeb1881abe50b9ee06d32c24b6ed
atamanahmet <ahmtataman@proton.me>
2025-04-04 12:31:13 +0300

    modified:   reverseString.java

--- reverseString.java
+++ reverseString.java
@@ -1,18 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-       if(n==0) return 0;
-       if(n==1) return 1;
-       if(n==2) return 2;
-       int first=1;
-       int second=2;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        int first = 1;
+        int second = 2;
 
-       for(int i = 0; i<n-2;i++){
+        for (int i = 0; i < n - 2; i++) {
             int temp = second;
-            second =first+second;
+            second = first + second;
             first = temp;
-       }
-       return second;
-
+        }
+        return second;
 
     }
 }
