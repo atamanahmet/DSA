cab221a99063d7e94b46c79accf9449de7d09ada
atamanahmet <ahmtataman@proton.me>
2025-04-11 13:31:11 +0300

    modified:   BinarySearchTree.java 	modified:   DeleteFromBST.java 	modified:   InsertIntoBST.java

--- InsertIntoBST.java
+++ InsertIntoBST.java
@@ -1,21 +1,22 @@
-import javax.swing.tree.TreeNode;
-
 public class InsertIntoBST {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     * int val;
-     * TreeNode left;
-     * TreeNode right;
-     * TreeNode() {}
-     * TreeNode(int val) { this.val = val; }
-     * TreeNode(int val, TreeNode left, TreeNode right) {
-     * this.val = val;
-     * this.left = left;
-     * this.right = right;
-     * }
-     * }
-     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     // Iterative
 
     class Solution {
